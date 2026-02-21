255fae8039b7c2764458eb35bba06f6c032f037a
Pinitha Savidya <savidyabalasooriya617@gmail.com>
2026-02-21 09:33:27 +0530

    Add invoice routes to main file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
 from user_routes import router as user_router
+from invoice_routes import router as invoice_router
 app = FastAPI(title="C-Clarke ORM Implementation with DB Agent")
 
 app.include_router(router=user_router)
+app.include_router(router=invoice_router)
