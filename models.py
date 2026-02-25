1c5d5622faa93970cb2f5af8b09b7d2dd7c7c38f
Pinitha Savidya <savidyabalasooriya617@gmail.com>
2026-02-25 14:16:01 +0530

    Add pending_requests models

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import String, func
+from sqlalchemy import String, func, Text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -22,3 +22,12 @@ class Invoice(Base):
     amount: Mapped[float] = mapped_column(nullable=False)
     description: Mapped[str] = mapped_column(String[255])
     created_at : Mapped[datetime] = mapped_column(server_default=func.now(), default=datetime.now())
+
+class PendingInsertRequest(Base):
+    __tablename__ = "pending_requests"
+
+    id: Mapped[str] = mapped_column(String(36), primary_key=True)
+    query: Mapped[str] = mapped_column(String(1000))
+    sql : Mapped[str] = mapped_column(Text())
+    status: Mapped[str] = mapped_column(String(20), default="pending")
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now, default=datetime.now())
